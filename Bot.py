361ee19a6080c6198b1aac56c78d4c4b4990d3f5
GhostLeader139 <74319769+GhostLeader139@users.noreply.github.com>
2020-11-11 14:53:39 -0500

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,10 +1,6 @@
 import discord
 import os
-import random
-from discord.ext import commands
 
 client = commands.Bot(command_prefix="p:")
-client.remove_command('help')
 
 client.run(os.environ['DISCORD_TOKEN'])
-
